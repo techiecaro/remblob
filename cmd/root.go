b73e0fda198651f9fa5910c846b8ab8821f85670
Karol Duleba <mr.fuxi@gmail.com>
2021-08-09 22:41:13 +0100

    Edit file from temp location

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,12 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
 
 	"github.com/spf13/cobra"
 )
@@ -24,4 +30,48 @@ func Execute() {
 
 func main(cmd *cobra.Command, args []string) {
 	fmt.Println("main called")
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	remoteEdit(f)
+}
+
+func runEditor(fn string) {
+	cmd := exec.Command("vim", fn)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func remoteEdit(src io.Reader) {
+	tmpDirName, err := ioutil.TempDir("", "remote-edit-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDirName)
+
+	tmpFileName := path.Join(tmpDirName, "tmp-file")
+
+	w, err := os.Create(tmpFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer w.Close()
+
+	// Copy the src to temp destination
+	n, err := io.Copy(w, src)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Copied %v bytes\n", n)
+
+	runEditor(tmpFileName)
+
+	fmt.Println("Temp dir name:", tmpFileName)
 }
