edebede3556d099f38c8388840e67a568033c703
Karol Duleba <mr.fuxi@gmail.com>
2021-08-31 21:52:42 +0100

    Start of getting the FileStorage dynamically

--- storage/interface.go
+++ storage/interface.go
@@ -1,5 +1,10 @@
 package storage
 
+import (
+    "fmt"
+    "net/url"
+)
+
 type FileStorage interface {
     Read(p []byte) (n int, err error)
     Write(p []byte) (n int, err error)
@@ -7,5 +12,17 @@ type FileStorage interface {
 }
 
 func GetFileStorage(uri string) FileStorage {
-    return getLocalFileStorage(uri)
+    parsedURL, err := url.Parse(uri)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("Scheme: ", parsedURL.Scheme)
+    fmt.Println("Path: ", parsedURL.Path)
+
+    switch parsedURL.Scheme {
+    case "":
+        return getLocalFileStorage(parsedURL.Path)
+    default:
+        panic("Can not handle this uri")
+    }
 }
